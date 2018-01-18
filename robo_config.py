77b9e50f73848735af3d76bc069348cc2adb3697
mariochampion <mario@mariochampion.com>
2018-01-18 17:08:19 -0600

    default to webstagram for DL source

--- robo_config.py
+++ robo_config.py
@@ -39,7 +39,7 @@ twilio_active = False # make True if you set up a twilio acct AND want to send t
 ## scrapesite_default can be:
 # "imgurapi" (requires you get API KEY from https://apidocs.imgur.com/) 
 # "webstagram" (which can be used anonymously)
-scrapesite_default = "imgurapi"
+scrapesite_default = "webstagram"
 
 
 # dynamically created files, etc
